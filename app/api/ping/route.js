ef4b784e94804268878be93d628abd5d75d583f6
miguel-stapleton <info@miguelstapleton.art>
2025-08-30 11:54:46 +0100

    Remove app directory causing build errors

--- app/api/ping/route.js
+++
@@ -1,8 +0,0 @@
-import { connectToDB } from "@/lib/mongodb";
-import Note from "@/models/Note";
-
-export async function GET() {
-  await connectToDB();
-  await Note.create({ title: "hello", body: "world" });
-  return Response.json({ ok: true });
-}
